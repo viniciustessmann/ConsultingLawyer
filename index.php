f59e1780a3e425a5a8b590a45c16e913aebd61f1
Vinícius Schlee Tessmann <viniciusschleetessmann@gmail.com>
2017-08-19 17:32:19 -0300

    add htaccess

--- index.php
+++ index.php
@@ -4,8 +4,14 @@ require_once 'vendor/autoload.php';
 
 $app = new Silex\Application();
 
+$app['debug'] = true;
+
 $app->get('/', function() use($app) {
     return 'Hello ';
+});
+
+$app->get('/about', function() use ($app) {
+	return 'About this site';
 });
 
 $app->run();
